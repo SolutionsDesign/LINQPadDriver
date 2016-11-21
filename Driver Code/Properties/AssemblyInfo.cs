0e687792078a7a7e5ad80cae700898bcea7df4dc
Frans Bouma <frans@sd.nl>
2016-11-21 13:28:05 +0100

    Migrated to v5.1 RTM

--- Driver Code/Properties/AssemblyInfo.cs
+++ Driver Code/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LLBLGen Pro LINQPad Driver")]
-[assembly: AssemblyDescription("LINQPad driver for LLBLGen Pro v5.0")]
+[assembly: AssemblyDescription("LINQPad driver for LLBLGen Pro v5.1")]
 [assembly: AssemblyConfiguration(".NET 3.5")]
 [assembly: AssemblyCompany("Solutions Design bv")]
 [assembly: AssemblyProduct("LLBLGen Pro")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyFileVersion("5.1.0")]
