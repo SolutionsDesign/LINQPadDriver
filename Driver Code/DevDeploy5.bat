13523f3a38a10c7c33000dda6aec73207f74f952
Miha Markič <miha@rthand.com>
2017-10-14 09:37:15 +0200

    Adds deploy for LINQPad 5

---
+++ Driver Code/DevDeploy5.bat
@@ -0,0 +1,13 @@
+@Echo off
+rem
+rem  You can simplify development by updating this batch file and then calling it from the 
+rem  project's post-build event.
+rem
+rem  It copies the output .DLL (and .PDB) to LINQPad's drivers folder, so that LINQPad
+rem  picks up the drivers immediately (without needing to click 'Add Driver').
+rem
+rem  The final part of the directory is the name of the assembly plus its public key token in brackets.
+echo 5.0 b094696bc21c000a
+xcopy /i/y *.dll "%localappdata%\LINQPad\Drivers\DataContext\4.6\SD.LLBLGen.Pro.LINQPadDriver53 (b094696bc21c000a)"
+xcopy /i/y *.pdb "%localappdata%\LINQPad\Drivers\DataContext\4.6\SD.LLBLGen.Pro.LINQPadDriver53 (b094696bc21c000a)"
+pause
